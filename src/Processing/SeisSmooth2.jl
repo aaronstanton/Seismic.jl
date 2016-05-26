795776b5975523c0efc9e9a6ce6247d3e0ff2995
Ubuntu <ubuntu@ip-172-31-33-244.us-west-2.compute.internal>
2016-05-26 19:06:29 +0000

    illum

--- src/Processing/SeisSmooth2.jl
+++ src/Processing/SeisSmooth2.jl
@@ -13,6 +13,16 @@ function SeisSmooth2(A,h::Array{Header,1};L1=7,L2=7)
 end
 
 function SeisSmooth2(in::ASCIIString,out::ASCIIString;key=["iaz";"iang"],L1=7,L2=7)
-	@compat parameters = Dict(:L1=>L1,:L2=>L2)
-	SeisProcess(in,out,[SeisSmooth2],[parameters];group="gather",key=key)
+
+	d,h,ext = SeisRead(in)
+	nang = size(d,3)
+	for iang = 1 : nang
+		A = squeeze(squeeze(squeeze(d[:,1,iang,1,:],2),2),2);
+		A = SeisSmooth2(A,L1=L1,L2=L2)
+		d[:,1,iang,1,:] = A
+	end
+	SeisWrite(out,d,h,ext)
+
+	#@compat parameters = Dict(:L1=>L1,:L2=>L2)
+	#SeisProcess(in,out,[SeisSmooth2],[parameters];group="gather",key=key)
 end
