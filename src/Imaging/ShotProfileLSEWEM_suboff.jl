ca62a7048f594ef5190746802c00755386340778
Aaron Stanton <astanton@Aarons-MacBook-Pro.local>
2016-11-17 13:26:38 -0700

    wd

--- src/Imaging/ShotProfileLSEWEM_suboff.jl
+++ src/Imaging/ShotProfileLSEWEM_suboff.jl
@@ -1,10 +1,14 @@
-function ShotProfileLSEWEM_suboff(m::Array{ASCIIString,1},d::Array{ASCIIString,1};Niter=10,mu=[0.;0.],cost="cost.txt",precon=false,Nsmooth=5,wm="NULL",pspi=true,nref=5,vp="vp.seis",vs="vs.seis",wav="wav.seis",sz=0.,gz=0.,npx=1,opx=0,dpx=1,nhx=1,ohx=0,dhx=1,fmin=0,fmax=80,padt=1,padx=1,verbose=false,sx=[0],kz_eps=0.5)
+function ShotProfileLSEWEM_suboff(m::Array{ASCIIString,1},d::Array{ASCIIString,1};Niter=10,mu=[0.;0.],cost="cost.txt",precon=false,Nsmooth=5,wm="NULL",wd="NULL",pspi=true,nref=5,vp="vp.seis",vs="vs.seis",wav="wav.seis",sz=0.,gz=0.,npx=1,opx=0,dpx=1,nhx=1,ohx=0,dhx=1,fmin=0,fmax=80,padt=1,padx=1,verbose=false,sx=[0],kz_eps=0.5)
 
 	# least squares shot profile wave equation migration of isotropic 2C data. 
 
 	rand_string = string(round(Int,rand()*100000))
-	wd = join(["tmp_LSM_wd_",rand_string])
-	CalculateSampling(d[1],wd,cutoff=1e-15)
+	wd_flag = true
+	if (wd == "NULL")
+	  wd = join(["tmp_LSM_wd_",rand_string])
+	  CalculateSampling(d[1],wd,cutoff=1e-15)
+	  wd_flag = false
+        end  
 	@compat param_wd = Dict(:w=>wd)
 	@compat param_mig = Dict(:pspi=>pspi,:nref=>nref,:vp=>vp,:vs=>vs,:wav=>wav,:sz=>sz,:gz=>gz,:nhx=>nhx,:ohx=>ohx,:dhx=>dhx,:fmin=>fmin,:fmax=>fmax,:padt=>padt,:padx=>padx,:verbose=>verbose,:sx=>sx,:kz_eps=>kz_eps)
 	@compat param_wm = Dict(:w=>wm)
@@ -35,7 +39,8 @@ function ShotProfileLSEWEM_suboff(m::Array{ASCIIString,1},d::Array{ASCIIString,1
 		SeisCopy(m_w,m)
 		SeisRemove(m_w)	
 	end
-
-	SeisRemove(wd)
+        if (wd_flag == false)
+	  SeisRemove(wd)
+        end
 
 end
